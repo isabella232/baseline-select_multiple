32873c21e3e6f03b99341ab70c552bf28bc4ff1d
Chuck King <cking@surveycto.com>
2020-02-25 21:14:26 -0500

    Apply code updates from baseline-select_one

--- source/script.js
+++ source/script.js
@@ -1,32 +1,33 @@
-// Find the input element
+// References to the elements in the field
+var choicesEl = document.getElementsByClassName("choice-container");
 var buttons = document.querySelectorAll('input[name="opt"]');
-var choiceContainers = document.getElementsByClassName("choice-container");
 
 // Define what happens when the user attempts to clear the response
+
 function clearAnswer() {
-    document.querySelector('input[name="opt"]:checked').checked = false;
+  // go through each choice
+  for (var i = 0; i < choicesEl.length; i++) {
+    choicesEl[i].classList.remove("chceked"); // remove the 'checked' class from the parent element
+    choicesEl[i].querySelectorAll('input').checked = false; // uncheck the checkbox input element
+    fieldProperties.CHOICES[i].CHOICE_SELECTED = false; // update the current answer 
+  }
 }
 
 // Save the user's response (update the current answer)
-function change(){
-  setAnswer(this.value);
-  // If the appearance is 'quick', then also progress to the next field
-  if ( fieldProperties.APPEARANCE.includes("quick") == true ) {
-    goToNextField();
-  }
-}
 
-// When a button is pressed, call the change() function and tell it which button was pressed.
-for(var i = 0; i < buttons.length; i++){
-  buttons[i].onchange = change;
-}
-
-// quick appearance
-if ( fieldProperties.APPEARANCE.includes("quick") == true ) {
-  // add the appearance-quick class to all the options
-  window.onload = function() {
-    for(var i = 0; i < choiceContainers.length; i++){
-      choiceContainers[i].classList.add("appearance-quick");
+function change() {
+  fieldProperties.CHOICES[this.value].CHOICE_SELECTED = !fieldProperties
+    .CHOICES[this.value].CHOICE_SELECTED;
+  var selected = [];
+  for (var i = 0; i < fieldProperties.CHOICES.length; i++) {
+    var choice = fieldProperties.CHOICES[i];
+    if (choice.CHOICE_SELECTED) {
+      selected.push(choice.CHOICE_INDEX);
     }
   }
-} 
+  setAnswer(selected.join(" "));
+}
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].onchange = change;
+}
