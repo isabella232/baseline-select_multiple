096902475f052723800e91d685549b80f3ba008a
wallace <wallace@surveycto.com>
2020-03-24 19:25:27 -0300

    SCTO-8222: Added support for RTL languages.

--- source/script.js
+++ source/script.js
@@ -1,6 +1,20 @@
 // References to the elements in the field
+var checkBoxesContainer = document.getElementById("checkboxes-container");
 var choicesEl = document.getElementsByClassName("choice-container");
 var buttons = document.querySelectorAll('input[name="opt"]');
+
+// Detect right-to-left languages
+function isRTL(s){
+  var ltrChars    = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF',
+      rtlChars    = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC',
+      rtlDirCheck = new RegExp('^[^'+ltrChars+']*['+rtlChars+']');
+
+  return rtlDirCheck.test(s);
+}
+
+if (fieldProperties.LANGUAGE !== null && isRTL(fieldProperties.LANGUAGE)) {
+  checkBoxesContainer.dir = "rtl";
+}
 
 // Define what happens when the user attempts to clear the response
 
